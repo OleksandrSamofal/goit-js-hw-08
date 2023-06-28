665f19ecf3ccacf775456ca6422641793c2505ae
Oleksandr Samofal <Skycruiser@i.ua>
2023-06-29 01:54:38 +0300

    four

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,4 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// console.log(galleryItems);
+console.log(galleryItems);
