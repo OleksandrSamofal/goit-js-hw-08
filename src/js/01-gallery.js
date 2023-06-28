d2382db65504de3a2e48521e9a0e4124fc248d3c
Oleksandr Samofal <Skycruiser@i.ua>
2023-06-29 02:43:07 +0300

    seven

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,57 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener("click", onImageClick);
+
+function createGalleryItemsMarkup(items) {
+    return galleryItems
+    .map(({ preview, original, description}) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}" rel='noreferrer noopener nofollow'>
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div> 
+        `;
+    })
+    .join('');
+};
+
+function onImageClick(event) {
+    event.preventDefault();
+
+    if(!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}'>
+    `);
+
+    instance.show();
+
+    window.addEventListener('keydown', onEscapePress);
+
+    function onEscapePress(event) {
+        if (event.code === 'Escape') {
+        instance.close();
+
+        window.removeEventListener('keydown', event);
+        }
+    };
+};
